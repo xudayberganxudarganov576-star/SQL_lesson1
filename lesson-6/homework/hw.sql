dd18f5b061cf0532ac6ed3ae70fca64a33097b97
xudayberganxudarganov576-star <xudayberganxudarganov576@gmail.com>
2025-09-20 12:35:08 +0500

    Create hw.sql
    Signed-off-by: xudayberganxudarganov576-star <xudayberganxudarganov576@gmail.com>

---
+++ lesson-6/homework/hw.sql
@@ -0,0 +1,99 @@
+
+
+
+
+| col1 | col2 |
+|------|------|
+| a    | b    |
+| a    | b    |
+| b    | a    |
+| c    | d    |
+| c    | d    |
+| m    | n    |
+| n    | m    |
+
+CREATE TABLE InputTbl (
+    col1 VARCHAR(10),
+    col2 VARCHAR(10)
+);
+    INSERT INTO InputTbl (col1, col2) VALUES 
+('a', 'b'),
+('a', 'b'),
+('b', 'a'),
+('c', 'd'),
+('c', 'd'),
+('m', 'n'),
+('n', 'm');
+
+SELECT DISTINCT
+    LEAST(col1, col2) AS col1,
+    GREATEST(col1, col2) AS col2
+FROM InputTbl;
+
+SELECT DISTINCT
+    CASE WHEN col1 < col2 THEN col1 ELSE col2 END AS col1,
+    CASE WHEN col1 < col2 THEN col2 ELSE col1 END AS col2
+FROM InputTbl;
+
+CREATE TABLE TestMultipleZero (
+    A INT NULL,
+    B INT NULL,
+    C INT NULL,
+    D INT NULL
+);
+
+INSERT INTO TestMultipleZero(A,B,C,D)
+VALUES 
+    (0,0,0,1),
+    (0,0,1,0),
+    (0,1,0,0),
+    (1,0,0,0),
+    (0,0,0,0),
+    (1,1,1,0);
+
+	select *
+	from TestMultipleZero
+	Where A<>0 or B<>0 or C<>0 or D<>0;
+
+	create table section1(id int, name varchar(20))
+insert into section1 values (1, 'Been'),
+       (2, 'Roma'),
+       (3, 'Steven'),
+       (4, 'Paulo'),
+       (5, 'Genryh'),
+       (6, 'Bruno'),
+       (7, 'Fred'),
+       (8, 'Andro')
+
+	   select * from section1
+	   where ID%2=1;
+
+	    select ID , name 
+		from section1
+		where ID=(select min(id) from section1);
+
+		select ID , name 
+		from section1
+		where ID=(select max(id) from section1);
+
+		select name
+		from section1 
+		where name like('b%');
+
+		CREATE TABLE ProductCodes (
+    Code VARCHAR(20)
+);
+
+INSERT INTO ProductCodes (Code) VALUES
+('X-123'),
+('X_456'),
+('X#789'),
+('X-001'),
+('X%202'),
+('X_ABC'),
+('X#DEF'),
+('X-999');
+
+select code 
+from ProductCodes
+where code like('%[_]%');
